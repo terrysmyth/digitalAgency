fd7b98774978fdfb005523fe8ed9aab8d28191fd
bonthesnek <xxroboxx6@gmail.com>
2019-06-10 18:02:49 +0800

    added ball movement + upgraded paddle controls/physics

--- public/JS/gameCtrl.js
+++ public/JS/gameCtrl.js
@@ -10,69 +10,124 @@ angular.module("myApp")
 		let rWidth = 25;
 		let rHeight = 80;
 		let rightRX = myCanvas.width - (5 + rWidth);
+		let bD = 20;
+		let bX = (myCanvas.width/2 - bD/2);
+		let bColor = "blue";
+		let bY = 100;
+		let dX = 2;
+		let dY = 2; 
 		let ctx = myCanvas.getContext("2d");
 		ctx.fillStyle = "#66FCF1";
+
+		let rUp = false;
+		let rDown = false;
+		let lUp = false;
+		let lDown = false;
+
+
 //left rectangle
 		const drawLeftRect = () => {
+			ctx.beginPath()
 			ctx.strokeRect(leftRX, leftRY, rWidth, rHeight);
 			ctx.fillRect(leftRX, leftRY, rWidth, rHeight);
 			ctx.stroke();
+			ctx.closePath();
 		}
 
 //right rectangle
 		const drawRightRect = () => {
+			ctx.beginPath()
 			ctx.strokeRect(rightRX, rightRY, rWidth, rHeight);
 			ctx.fillRect(rightRX, rightRY, rWidth, rHeight);
 			ctx.stroke();
+			ctx.closePath();
 		}
+
+		const drawCircle = () => {
+			ctx.beginPath();
+			ctx.arc(bX, bY, bD,0,2*Math.PI);
+			ctx.fillStyle = bColor;
+			ctx.fill();
+			ctx.closePath();
+		}
+
 
 		const keyDownHandler = (e) =>{
 			
 
-			if (e.keyCode == 13) {
-				myCanvas.style.backgroundImage = "url()";
-			}else if (e.keyCode == 79 ) {
-				rightRY -= 30;
+			if (e.keyCode == 79 ) {
+				rUp = true;
 			}else if (e.keyCode == 76 ) {
-				rightRY += 30;
+				rDown = true;
 			}else if (e.keyCode == 87 ) {
-				leftRY -= 30;
+				lUp = true;
 			}else if (e.keyCode == 83 ) {
-				leftRY +=30;
+				lDown = true;
 			};
+		};
 
+		const keyUpHandler = (e) =>{
+			
 
-// keycode 87 = w      83 = s
-
+			if (e.keyCode == 79 ) {
+				rUp = false;
+			}else if (e.keyCode == 76 ) {
+				rDown = false;
+			}else if (e.keyCode == 87 ) {
+				lUp = false;
+			}else if (e.keyCode == 83 ) {
+				lDown = false;
+			};
 		};
 
 
 		setInterval(function() {
 
-			while (rightRY < 0) {
-				rightRY += 10;
-			}
-			while (rightRY > (myCanvas.height - rHeight) ) {
+			if (rUp && rightRY >= 0) {
 				rightRY -= 10;
 			}
-			while (leftRY < 0) {
-				leftRY += 10;
+			if (rDown && rightRY < (myCanvas.height - rHeight) ) {
+				rightRY += 10;
 			}
-			while (leftRY > (myCanvas.height - rHeight) ) {
+			if (lUp && leftRY > 0) {
 				leftRY -= 10;
 			}
+			if (lDown && leftRY < (myCanvas.height - rHeight) ) {
+				leftRY += 10;
+			}
+
+			if (bX >= myCanvas.width - bD || bX <= 0+bD) {
+				dX = -dX;
+			}
+
+			if (bY <= 0+bD || bY >= myCanvas.height - bD ) {
+				dY= -dY;
+			}
+
+			
+		
+
+
+
+
 			runGame();
 
-		}, 100)
+		}, 10)
 
 		const runGame = () => {
-			ctx.clearRect(0, 0, myCanvas.width, myCanvas.height)
+			ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+			bX += dX;
+			bY += dY
 
-			drawRightRect();
-			drawLeftRect();
+			drawRightRect(rightRX, rightRY);
+			drawLeftRect(leftRX, leftRY);
+			drawCircle(bX, bY);
+
+
 		}
 
 		document.addEventListener("keydown", keyDownHandler);
+		document.addEventListener("keyup", keyUpHandler);
 
 
 
